d445514518eb5740b1994396249f39987d6e9f28
Sean Duiser <duzer84@gmail.com>
2022-03-29 23:00:43 -0400

    assignment fully functions. Display all employees needs improvement

--- app.js
+++ app.js
@@ -88,22 +88,155 @@ const viewAllEmployees = () => {
 };
 
 const addNewDepartment = () => {
-    db.query('INSERT INTO movies SET ?', req.body, function (err, result) {
-        console.table(result);
-        res.json(result);
-      });
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of the new department?'
+            }
+        ])
+        .then(name => {
+            connection.query(`INSERT INTO department SET ?`, name);
+            viewAllDepartments();
+        })
 };
 
 const addNewRole = () => {
-
+    connection.promise().query(`SELECT department.id, department.name FROM department;`)
+        .then(([allDepartments]) => {
+            let selectDepartment = allDepartments.map(({
+                id,
+                name
+            }) => ({
+                name: name,
+                value: id
+            }));
+            inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        name: 'title',
+                        message: 'What is the name of the new role?'
+                    },
+                    {
+                        type: 'list',
+                        name: 'department',
+                        message: 'Which department is the new role a part of?',
+                        choices: selectDepartment
+                    },
+                    {
+                        type: 'input',
+                        name: 'salary',
+                        message: 'What is the salary of the new role?'
+                    }])
+                .then(({ title, department, salary }) => {
+                    connection.query(`INSERT INTO role SET ?`, {
+                        title: title,
+                        department_id: department,
+                        salary: salary
+                    });
+                    viewAllRoles();
+                })
+        })
 };
 
 const addNewEmployee = () => {
-
+    connection.promise().query(`SELECT role.id, role.title FROM role;`)
+        .then(([allRoles]) => {
+            let selectRole = allRoles.map(({
+                id,
+                title
+            }) => ({
+                value: id,
+                name: title
+            }))
+            connection.promise().query(`SELECT employee.id, CONCAT(employee.first_name,' ',employee.last_name) AS manager FROM employee;`)
+                .then(([potentialManagers]) => {
+                    let selectManager = potentialManagers.map(({
+                        id,
+                        manager
+                    }) => ({
+                        value: id,
+                        name: manager
+                    }));
+                    inquirer
+                        .prompt([
+                            {
+                                type: 'input',
+                                name: 'firstName',
+                                message: 'What is the first name of the new employee?'
+                            },
+                            {
+                                type: 'input',
+                                name: 'lastName',
+                                message: 'What is the last name of the new employee?'
+                            },
+                            {
+                                type: 'list',
+                                name: 'role',
+                                message: 'Which role is the new employee assigned to?',
+                                choices: selectRole
+                            },
+                            {
+                                type: 'list',
+                                name: 'manager',
+                                message: 'Who is the manager of the new employee?',
+                                choices: selectManager
+                            }])
+                        .then(({ firstName, lastName, role, manager }) => {
+                            connection.query(`INSERT INTO employee SET ?`, {
+                                first_name: firstName,
+                                last_name: lastName,
+                                role_id: role,
+                                manager_id: manager
+                            });
+                            viewAllEmployees();
+                        })
+                })
+        })
 };
 
 const updateRole = () => {
-
+    connection.promise().query(`SELECT employee.id, CONCAT(employee.first_name,' ',employee.last_name) AS name FROM employee;`)
+        .then(([currentEmployees]) => {
+            let employeeSelection = currentEmployees.map(({
+                id,
+                name
+            }) => ({
+                name: name,
+                value: id
+            }));
+            connection.promise().query(`SELECT role.id, role.title FROM role;`)
+                .then(([currentRoles]) => {
+                    let roleSelection = currentRoles.map(({
+                        id,
+                        title
+                    }) => ({
+                        name: title,
+                        value: id
+                    }));
+                    inquirer
+                        .prompt([
+                            {
+                                type: 'list',
+                                name: 'selectEmployee',
+                                message: 'Which employee would you like to select?',
+                                choices: employeeSelection
+                            },
+                            {
+                                type: 'list',
+                                name: 'selectNewRole',
+                                message: 'What is the new role for this employee?',
+                                choices: roleSelection
+                            }
+                        ])
+                        .then(({ selectEmployee, selectNewRole }) => {
+                            connection.query(`UPDATE employee SET role_id = ${selectNewRole} WHERE id = ${selectEmployee}`);
+                            viewAllEmployees();
+                        })
+                })
+        })
 };
 
 mainMenu();
